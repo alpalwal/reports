ca160eaabb51086e4418352699136208c09f09b7
alpalwal <alex@abcwebdesigner.com>
2017-02-06 19:35:04 -0800

    final push - updated comments / took out jwt

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 /*
-ping API every hour - 24h
-if message > send email
-http://api.38plank.com/v1/reports
-var JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkFuZHJldyIsImVtYWlsIjoid2h5YnRocjIzMkBnbWFpbC5jb20iLCJpYXQiOjE0ODU5OTc0NTN9.WTpThgcPhQB6OJLeRd3A21ZxoZP7RO35n7rwATwKF-8';
+Set the CloudWatch event to run every 24h
 
-Authorization: 'JWT <jwt>'
+REQUIRED: JWT env variable.
+Substitute your SNS ARN for the one shown
 */
-//const JWT = process.env.JWT; //Auth token. Don't want to keep it in here
 
-//JWT hard coded for now. remove later
+const JWT = process.env.JWT; //Auth token. Don't want to keep it in here
 var AWS = require("aws-sdk");
 var request = require('request');
-const JWT ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkFuZHJldyIsImVtYWlsIjoid2h5YnRocjIzMkBnbWFpbC5jb20iLCJpYXQiOjE0ODU5OTc0NTN9.WTpThgcPhQB6OJLeRd3A21ZxoZP7RO35n7rwATwKF-8";
 const URL = 'http://api.38plank.com/v1/reports';
 
 var options = {
   url: URL,
   headers: {
     Authorization: "JWT " + JWT
+  }
+};
+
+function hitApi(error, response, body) {
+    if (!error && response.statusCode == 200) {
+    var info = JSON.parse(body);
+
+    for (var i = 0; i < info.data.length; i++) {
+        var report = info.data[i];
+        var elapsedTime = (new Date() - report.dateReported)
+        //3600000 ms in an hour, 86400000 in a day
+        if (elapsedTime && elapsedTime < 86400000) {
+            console.log("new report!" + report)
+            postToSns(report);
+        }
+    }
   }
 };
 
@@ -31,22 +43,6 @@ var postToSns = function(event) {
         TopicArn: "arn:aws:sns:us-west-2:621958466464:andrew_reports"
     };
     sns.publish(params, function(error,data){console.log(error + data);});
-};
-
-function hitApi(error, response, body) {
-    if (!error && response.statusCode == 200) {
-    var info = JSON.parse(body);
-
-    for (var i = 0; i < info.data.length; i++) {
-        var report = info.data[i];
-        var elapsedTime = (new Date() - report.dateReported)
-      //3600000 ms in an hour, 86400000 in a day
-        if (elapsedTime && elapsedTime < 3600000) {
-            console.log("new report!" + report)
-            postToSns(report);
-        }
-    }
-  }
 };
 
 exports.handler = (event, context, callback) => {
